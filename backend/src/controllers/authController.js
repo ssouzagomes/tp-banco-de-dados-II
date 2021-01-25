13a99fdd0f6b6120cca873d2d9ec2070bb213aad
Lucas Almeida <42757403+UFOP-LucasAlmeida@users.noreply.github.com>
2021-01-25 20:00:47 -0300

    init no backend

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const user = require('../models/user');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    const { email } = req.body;
+  
+    try {
+      if (await User.findOne({ email }))
+        return res.status(400).send({ error: 'User already exists' });
+  
+      const u = await user.create(req.body);
+  
+      u.password = undefined;
+  
+      return res.send({
+        user,
+        token: generateToken({ id: user.id }),
+      });
+    } catch (err) {
+      return res.status(400).send({ error: 'Registration failed' });
+    }
+  });
+
+module.exports = app => app.use('/auth', router);
