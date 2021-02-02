44f1dd1d99d44505162a4b5479a1c623cdad4460
Samuel Souza <samuel.souzagomes001@gmail.com>
2021-02-02 15:58:30 -0300

    Page 'Welcome' created

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  ContentContainer,
+  Content,
+} from './styles';
+
+
+const Login: React.FC = () => {
+
+  return (
+    <>
+      <ContentContainer>
+        <Content>
+          
+        </Content>
+      </ContentContainer>
+    </>
+  );
+};
+export default Login;
