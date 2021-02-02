47e0318af2aa9955994da32c31aabc9fbc97c586
Samuel Souza <samuel.souzagomes001@gmail.com>
2021-02-01 21:57:33 -0300

    create react app

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, BrowserRouter } from 'react-router-dom'
+
+// import Welcome from './pages/Welcome'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      {/* <Route component={Welcome} path="/" exact /> */}
+    </BrowserRouter>
+  )
+}
+
+export default Routes
