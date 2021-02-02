e5a892086c34fd47a2b9f1c3b2f82043b294c258
Lucas Almeida <42757403+UFOP-LucasAlmeida@users.noreply.github.com>
2021-02-02 17:01:09 -0300

    agr ta funcionando

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const uri =
-    "mongodb+srv://admin:admin@cluster0-shard-00-02.tauho.mongodb.net/rentx/rentx?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:admin@cluster0.tauho.mongodb.net/rentx?retryWrites=true&w=majority";
 try {
     mongoose.connect( uri, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
     console.log("connected"));    
-    }catch (error) { 
+    } catch (error) { 
     console.log("could not connect");    
 }
 
