f7a670faaf3dbbacbcff5054717f972c660a8dab
ldalmeida <ldalmeida.br@gmail.com>
2021-04-01 12:28:05 -0300

    mudando de post pra get

--- backend/src/app/controllers/listingController.js
+++ backend/src/app/controllers/listingController.js
@@ -49,8 +49,6 @@ exports.getListingsOnRequestedRange = async function(req, res){
   const { priceStart } = req.body
   const { priceEnd } = req.body
 
-  console.log(startDate, endDate)
-
   if(!endDate && endDate){
     return res.status(400).send({ error: 'Data de inico e fim nao especificada'});
   }
